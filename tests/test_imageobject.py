4a6c34446dc0919805529aba24ce8b6ec835690b
Sahil Singh <sahilsingh90@gmail.com>
2019-08-29 19:49:27 +0530

    Added unit tests for ImageObject

--- tests/test_imageobject.py
+++ tests/test_imageobject.py
@@ -3,22 +3,22 @@ from pathlib import Path
 import os
 import numpy as np
 
+TEST_PATH = Path.cwd().joinpath('tests', 'test_data', 'imageobject')
+
+img = ImageObject.frompath(str(TEST_PATH.joinpath('hs_smooth.png')))
+occ = ImageObject.frompath(str(TEST_PATH.joinpath('occ0.jpg')))
+
 def test_frompath():
-    img = ImageObject.frompath(
-        str(Path.cwd().joinpath(
-            'tests',
-            'test_data',
-            'imageobject',
-            'hs_smooth.png')))
     assert isinstance(img, ImageObject)
 
 def test_findmask():
-    img = ImageObject.frompath(
-        str(Path.cwd().joinpath(
-            'tests',
-            'test_data',
-            'imageobject',
-            'hs_smooth.png')))
     boundRect, mask = img.find_mask()
     assert len(boundRect) == 4
     assert isinstance(mask, np.ndarray)
+
+def test_add_occlusion():
+    assert img.shape == img.add_occlusion(occ).shape
+
+def test_augment_object():
+    assert img.shape == img.augment_obj().shape
+
