29de929954e345ed63a76ac11dac0d0a47a9b54f
Sahil Singh <sahilsingh90@gmail.com>
2019-09-02 16:39:36 +0530

    Added filehandler to logging.

--- xzgen/__init__.py
+++ xzgen/__init__.py
@@ -2,3 +2,12 @@ import logging
 from .imageconfig import ImageData, Dimension
 from .imageobject import ImageObject
 from .scene import Scene
+
+logging.basicConfig(level=logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh = logging.FileHandler('debug.log')
+fh.setFormatter(formatter)
+
+logger = logging.getLogger('xzgen')
+logger.addHandler(fh)
+
