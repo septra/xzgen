3193f55c5be8b77cd295102a2bde6a4deee6f646
Sahil Singh <sahilsingh90@gmail.com>
2019-09-03 12:58:39 +0530

    Set default log level to info

--- xzgen/__init__.py
+++ xzgen/__init__.py
@@ -3,7 +3,7 @@ from .imageconfig import ImageData, Dimension
 from .imageobject import ImageObject
 from .scene import Scene
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh = logging.FileHandler('debug.log')
 fh.setFormatter(formatter)
